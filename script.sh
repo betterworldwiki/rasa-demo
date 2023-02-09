5ba9909052ca5945179a718b48f2f862a3424807
betterworldwiki <105317914+betterworldwiki@users.noreply.github.com>
2023-02-09 12:26:20 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-if not [ -d "/app/models" ]; then
+if [ -d "/app/models" ]; then
+  echo "Directoy /app/models exists."
+else
   echo "Installing and training model files in /app/models..."
   rasa train --augmentation 0
   echo "Model Training successful! (/app/models)"
